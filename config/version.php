191094092a7cfd6d04be472510e92ace2f5b15a3
thanhcong1710 <thanhcong1710@gmail.com>
2020-07-02 17:32:04 +0700

    update

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.9.3',
+  'full_app_version' => 'v4.9.3 - build 4395-g9a2440dc4',
+  'build_version' => '4395',
+  'prerelease_version' => '',
+  'hash_version' => 'g9a2440dc4',
+  'full_hash' => 'v4.9.3-42-g9a2440dc4',
+  'branch' => 'master',
+);
